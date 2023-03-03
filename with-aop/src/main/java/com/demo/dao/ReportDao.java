192f50a70b6422e8a72f2b3323b58b1c3559a069
Kyaw Kyaw Lwin <kyawkyawlwinthant@gmail.com>
2023-03-03 13:15:07 +0630

    benifit of aop

---
+++ with-aop/src/main/java/com/demo/dao/ReportDao.java
@@ -0,0 +1,22 @@
+package com.demo.dao;
+
+import com.demo.annotation.Logger;
+import com.demo.ds.Report;
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.Target;
+
+@Component @Logger
+public class ReportDao {
+
+    public void save(Report report){
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(report.getClass()
+                .getSimpleName() +" saved.");
+    }
+}
